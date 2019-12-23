21938166dc9453d75ae0a7d96bbbae6b339c6399
jaakofalltrade <jaako.andes@boom.camp>
2019-12-23 14:57:58 +0800

    Finished Django app, part 2

--- polls/models.py
+++ polls/models.py
@@ -7,6 +7,10 @@ from django.utils import timezone
 class Question(models.Model):
     def __str__(self):
         return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
