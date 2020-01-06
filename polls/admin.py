122ee857fb3ec7209febb9aa7bb0eb216a92f49d
jaakofalltrade <jaako.andes@boom.camp>
2020-01-06 18:02:13 +0800

    Finished Django App 6 still working on 7 and redesign.

--- polls/admin.py
+++ polls/admin.py
@@ -3,6 +3,13 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
-# Register your models here.
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    list_display = ('question_text', 'pub_date')
+    list_filter = ['pub_date']
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
